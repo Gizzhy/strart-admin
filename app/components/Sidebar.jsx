7247fb11876fc5d9c906be01466520e02311b5c8
Gizzhy <gizzhyakinz@gmail.com>
2025-01-06 23:17:51 +0100

    sidebar built

---
+++ app/components/Sidebar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "./sidebar.module.scss";
+import Image from "next/image";
+import logo from "../../assets/strart2.png";
+import { RiLogoutBoxFill } from "react-icons/ri";
+import { FaFileUpload } from "react-icons/fa";
+import { GoHomeFill } from "react-icons/go";
+
+const Sidebar = ({ setSelectedPage, selectedPage }) => {
+  const getActiveClass = (page) => (selectedPage === page ? styles.active : "");
+
+  return (
+    <>
+      <aside className={styles.sidebar}>
+        <div className={styles.sidebarHead}>
+          <Image src={logo} alt="dejyle" className={styles.logo} />
+          {/* <span className={styles.dej}>Dejyle Venture</span> */}
+        </div>
+        <hr className={styles.line} />
+        <div className={styles.sidebarLinks}>
+          <div
+            className={`${styles.view} ${getActiveClass("View")}`}
+            onClick={() => setSelectedPage("View")}
+          >
+            <GoHomeFill className={styles.sidebarIcons} />
+            <span className={styles.sidebarNav}>View products</span>
+          </div>
+          <div
+            className={`${styles.upload} ${getActiveClass("Upload")}`}
+            onClick={() => setSelectedPage("Upload")}
+          >
+            <FaFileUpload className={styles.sidebarIcons} />
+            <span className={styles.sidebarNav}>Upload products</span>
+          </div>
+          <div
+            className={`${styles.logout} ${getActiveClass("Logout")}`}
+            onClick={() => setSelectedPage("Logout")}
+          >
+            <RiLogoutBoxFill className={styles.sidebarIcons} />
+            <span className={styles.sidebarLogout}>Logout</span>
+          </div>
+        </div>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,44 @@
 "use client";
 
 import withAuth from "@/utils/withAuth";
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Upload from "./sidebarcomp/Upload/Upload";
+import Logout from "./sidebarcomp/Logout/Logout";
+import View from "./sidebarcomp/View/View";
+import styles from "./home.module.scss";
 
 function Home() {
-  return <div>Welcome to the Admin Panel</div>;
+  const [selectedPage, setSelectedPage] = useState("Upload");
+  const renderPage = () => {
+    switch (selectedPage) {
+      // case "Overview":
+      //   return <Body navigateToPage={navigateToPage} />;
+      case "Upload":
+        return <Upload />;
+      case "View":
+        return <View />;
+      case "Logout":
+        return <Logout />;
+      default:
+        return <View />;
+    }
+  };
+  return (
+    <>
+      <Sidebar setSelectedPage={setSelectedPage} selectedPage={selectedPage} />
+      <main className={styles.mainPage}>{renderPage()}</main>
+    </>
+  );
 }
 
 export default withAuth(Home);
+
+// export const Dashboard = () => {
+//   const navigateToPage = (page, tab) => {
+//     if (page === "Messages" && tab) {
+//       localStorage.setItem("selectedTab", tab); // Store the selected tab
+//     }
+//     setSelectedPage(page);
+//   };
+// };
