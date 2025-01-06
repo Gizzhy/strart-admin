c8ac917e21815768e5515887c1a2da7f04e6c863
Gizzhy <gizzhyakinz@gmail.com>
2025-01-06 23:29:26 +0100

    google fonts corrections

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,4 @@
 import "./globals.css"; // Make sure to include your global CSS
-import { Poppins } from "@next/font/google";
-
-const poppins = Poppins({
-  subsets: ["latin"], // You can add other subsets if needed
-  weight: ["400", "500", "600", "700"], // Add font weights as required
-  style: ["normal"], // You can also include 'italic' if necessary
-});
 
 export const metadata = {
   title: "Strart Admin Dashboard",
@@ -14,7 +7,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={poppins.className}>
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
